629f6c9d9b85f00099fa00632e5882b1045da792
Virmon K. Tumulak <virmontumulak@gmail.com>
2018-11-22 22:38:10 +0800

    added dashboard

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Table, Icon, message, Popconfirm, Card, Col, Row } from 'antd';
+
+class Dashboard extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Dashboard</h2>
+                {/* <div className="widget">
+                    Ongoing Projects
+                </div>
+                <div className="widget">
+                    Pending Projects
+                </div>
+                <div className="widget">
+                    Accomplished Projects
+                </div>
+                <div className="widget">
+                    No. of Clients
+                </div> */}
+                <Row gutter={16}>
+                    <Col span={8}>
+                        <Card title="ONGOING PROJECTS" bordered={false}>10</Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card title="ACCOMPLISHED PROJECTS" bordered={false}>50</Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card title="NO. OF CLIENTS" bordered={false}>20</Card>
+                    </Col>
+                </Row><br/>
+                <Row gutter={16}>
+                    <Col span={8}>
+                        <Card title="TOTAL SALES" bordered={false}>600,000</Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card title="LESS TAX" bordered={false}>100,000</Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card title="NET INCOME" bordered={false}>500,000</Card>
+                    </Col>
+                </Row><br/>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
